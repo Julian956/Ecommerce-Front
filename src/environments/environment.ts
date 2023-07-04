d10805f68d83c8a68db7aec277cd6bade3e23f4d
Julian Andres <39501445+Julian956@users.noreply.github.com>
2023-07-04 16:06:13 -0500

    firebase implement and buttons responsive

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'imagens-product',
+    appId: '1:958320105183:web:d73fa2b873ce026b57fb6a',
+    storageBucket: 'imagens-product.appspot.com',
+    apiKey: 'AIzaSyBvHzlMDhYyO_42wYA5lKvOnpbUtrzXbOo',
+    authDomain: 'imagens-product.firebaseapp.com',
+    messagingSenderId: '958320105183',
+    measurementId: 'G-K0HWWP2YCS',
+  },};
